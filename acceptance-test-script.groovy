bd83cb98e8390b3c04057a2c339bb74d59cec4f2
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-02-19 11:45:09 +0100

    Simplify example

--- acceptance-test-script.groovy
+++ acceptance-test-script.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 @GrabResolver(name='jitpack', root='https://jitpack.io', m2Compatible='true')
+@Grab(group='org.testcontainers', module='testcontainers', version='1.6.0')
 @Grab('com.github.testcontainers:testcontainers-groovy-script:1.4.2')
 @Grab(group='io.rest-assured', module='rest-assured', version='3.0.7', scope='test')
 @GrabExclude('org.codehaus.groovy:groovy-xml')
@@ -12,36 +13,35 @@ import static io.restassured.RestAssured.*
 import static org.hamcrest.Matchers.*
 import static org.testcontainers.containers.Network.newNetwork;
 
-class TestNetwork {
-    static Network testNetwork = Network.newNetwork()
-}
+@Test
+void "I will fail"() {
+    // setup
+    Network testNetwork = Network.newNetwork()
+    GenericContainer apache = new GenericContainer("httpd:2.4")
+            .withExposedPorts(80)
+            .withNetwork(testNetwork)
+            .withNetworkAliases("apache")
 
-@groovy.transform.Field
-static public GenericContainer nginx = new GenericContainer("nginx:1.9.4")
-        .withClasspathResourceMapping("default.conf", "/etc/nginx/conf.d/default.conf", BindMode.READ_ONLY)
-        .withExposedPorts(80)
-        .withNetwork(TestNetwork.testNetwork)
-
-@groovy.transform.Field
-static public GenericContainer apache = new GenericContainer("httpd:2.4")
-        .withExposedPorts(80)
-        .withNetwork(TestNetwork.testNetwork)
-        .withNetworkAliases("apache")
+    GenericContainer nginx = new GenericContainer("nginx:1.9.4")
+            .withClasspathResourceMapping("default.conf", "/etc/nginx/conf.d/default.conf", BindMode.READ_ONLY)
+            .withExposedPorts(80)
+            .withNetwork(testNetwork)
 
 
-@BeforeClass
-static void setup() {
     apache.start()
     nginx.start()
 
     RestAssured.baseURI = "http://${nginx.containerIpAddress}:${nginx.firstMappedPort}"
-}
 
-@Test
-void "I will fail"() {
+    // test
     when()
             .get("/")
     .then()
             .statusCode(200)
             .body(containsString("It works!"))
+
+
+    // cleanup
+    apache.stop()
+    nginx.stop()
 }
