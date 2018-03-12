a81d1047ae5bce201f6246c27eb44355e617dfa3
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-03-12 20:12:03 +0100

    Use JUnit4 instead of Spock for first example

--- src/test/groovy/com/greachconf/groovyintegrationtests/BookRepositorySpec.groovy
+++ src/test/groovy/com/greachconf/groovyintegrationtests/BookRepositorySpec.groovy
@@ -1,62 +1,66 @@
 package com.greachconf.groovyintegrationtests
 
-import com.groovycoder.spockdockerextension.Testcontainers
-import spock.lang.Specification
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
-@Testcontainers
-class BookRepositorySpec extends Specification {
+class BookRepositorySpec {
 
     BookRepository bookRepository
 
-    def setup() {
+    @Before
+    void setup() {
         bookRepository = new BookRepository("jdbc:h2:./test",
                 "sa",
                 "")
         bookRepository.init()
     }
 
-    def cleanup() {
+    @After
+    void cleanup() {
         new File("test.mv.db").delete()
     }
 
-    def "empty repository is empty"() {
-        expect:
-        bookRepository.count() == 0
+    @Test
+    void "empty repository is empty"() {
+        assert bookRepository.count() == 0
     }
 
-    def "repository contains one book after saving it"() {
-        given: "the repo"
+    @Test
+    void "repository contains one book after saving it"() {
         def repo = bookRepository
-
-        and: "a book"
         def book = new Book("Moby Dick", "Herman Melville")
 
-        when: "saving it"
         repo.save(book)
 
-        then: "repo contains one book"
-        repo.count() == 1
+        assert repo.count() == 1
     }
 
-    def "repository finds books of given author"() {
+    @Test
+    void "repository contains another book after saving it"() {
+        def repo = bookRepository
+        def book = new Book("The Colour of Magic", "Terry Pratchett")
+
+        repo.save(book)
+
+        assert repo.count() == 1
+    }
+
+    @Test
+    void "repository finds books of given author"() {
         given: "the repo"
         def repo = bookRepository
 
-        and: "some books"
         def mobyDick = new Book("Moby Dick", "Herman Melville")
         def terryPratchett = "Terry Pratchett"
         def magic = new Book("The Colour of Magic", terryPratchett)
         def elephant = new Book("The Fifth Elephant", terryPratchett)
 
-        when: "saving them"
         [mobyDick, magic, elephant].each { repo.save(it) }
-
-        and: "searching for books of author"
         def foundBooks = repo.findAllByAuthor(terryPratchett)
 
-        then: "expected books were found"
-        foundBooks.size() == 2
-        foundBooks.containsAll([magic, elephant])
+        assert foundBooks.size() == 2
+        assert foundBooks.containsAll([magic, elephant])
     }
 
 
